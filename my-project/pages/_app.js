e41b633144a8842c3ef41a88d141c9a1c10cf33a
Calic Bojan (s) <bojan.calic@students.fhnw.ch>
2022-08-02 21:38:22 +0200

    - Login with Case Manager as user and first component of Dashboard for Case Manager

--- my-project/pages/_app.js
+++ my-project/pages/_app.js
@@ -1,7 +1,49 @@
 import '../styles/globals.css'
+import React, { useState } from "react";
+import Home from "./index";
+import DashCaseManager from "./components/DashCaseManager";
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const caseManagerUser = {
+    username: "Case Manager",
+    password: "test123"
+  }
+  const [user, setUser] = useState({username:"", password:""});
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    console.log(details);
+    if(details.username == caseManagerUser.username && details.password == caseManagerUser.password){
+      console.log("Logged in as Case Manager");
+      setUser({
+        username: details.username,
+        password: details.password
+      })
+    }else{
+      console.log("Details do not match");
+      setError("Nutzername oder Passwort falsch")
+    }
+  }
+
+  const Logout = () => {
+    console.log("Logout");
+    setUser({
+      username: "",
+      password: ""
+    })
+
+  }
+
+  return(
+      <div className="App">
+        {(user.username != "") ? (
+            <DashCaseManager />
+        ):(
+            <Home Login={Login} error={error}/>
+        )}
+      </div>
+  );
 }
 
 export default MyApp
---
+++ my-project/pages/components/DashCaseManager.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function DashCaseManager() {
+    return (
+        <div>
+            <h1>
+                Welcome to the Dashboard of the Case Manager
+            </h1>
+        </div>
+    );
+}
+
+export default DashCaseManager;
